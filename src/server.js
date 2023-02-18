35f79472ad535f9bb4fe25e82c8c3e8158d7fa0f
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-18 19:02:35 -0200

    created src/server.js

---
+++ src/server.js
@@ -0,0 +1,17 @@
+require('express-async-errors')
+const appError = require('./utils/appError')
+
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+app.use(( error, request, response, next ) => {
+    if (error instanceof appError) {
+        return response.status(error.status).json(error.message)
+    }
+    return response.status(500).json(`Internal Server Error :> ${error.message}`)
+})
+
+const PORT = 3333
+app.listen(PORT, console.log(PORT))
