4494897748391e4c1de44ce2244be9cbdd2b3c39
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-19 14:37:59 -0200

    UsersController.js created SR_func updateEmailCheck{}

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -7,8 +7,25 @@ class UsersController {
         const { name, email, currentPassword, newPassword } = request.body
         const { id } = request.params
 
-        
+        const user = await knex('users').where({id}).first()
+
+        if (await updateEmailCheck(email, user)) {
+            user.email = email
+        }
     }
 }
 
 module.exports = UsersController
+
+async function updateEmailCheck (email, user) {
+    if (email) {
+
+        const userWithEmail = await knex('users').where({email}).first() 
+
+        if (userWithEmail && userWithEmail.id !== user.id) {
+            throw new appError (" This email is not available!")
+        }
+        return true
+    }
+    return false
+}
