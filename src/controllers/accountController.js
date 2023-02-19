30c7d2d193ceed14bc708b7343b4b032f7370082
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-18 22:00:13 -0200

    accountController.js created method signUp{}

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -2,8 +2,27 @@ const knex = require('../database/knex')
 
 const appError = require('../utils/appError')
 
-class accountController {
+const { hash } = require('bcryptjs')
 
+class accountController {
+    async signIn ( request, response ) {
+        const { name, email, password, admin_key } = request.body
+
+        if (admin_key && admin_key !== 'admin_key123') {
+            throw new appError(`You don't have permission to create an admin account.`)
+        }
+
+        const userWithEmail = await knex('users').where({email}).first()
+        if (userWithEmail) {
+            throw new appError(`This email: ${email}, it's already in use.`)
+        }
+
+        const cryptedPassword = await hash( password, 8 )
+
+        await knex('users').insert({ name, email, password: cryptedPassword, admin_key })
+
+        return response.status(201).json()
+    }
 }
 
 module.exports = accountController
