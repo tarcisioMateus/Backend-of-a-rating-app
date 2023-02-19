8de741f0c7bf45a050f4697dca84cd8b5b77510e
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-19 13:54:35 -0200

    UserController.js created method update{}

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,7 +3,12 @@ const knex = require('../database/knex')
 const appError = require('../utils/appError')
 
 class UsersController {
+    async update (request, response) {
+        const { name, email, currentPassword, newPassword } = request.body
+        const { id } = request.params
 
+        
+    }
 }
 
 module.exports = UsersController
