bc1b44c2712e27c2c389cef232059a28db03bd1c
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-19 14:51:35 -0200

    UsersController.js created SR_func updatePasswordCheck{}

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,6 +2,8 @@ const knex = require('../database/knex')
 
 const appError = require('../utils/appError')
 
+const { hash, compare } = require('bcryptjs')
+
 class UsersController {
     async update (request, response) {
         const { name, email, currentPassword, newPassword } = request.body
@@ -11,6 +13,10 @@ class UsersController {
 
         if (await updateEmailCheck(email, user)) {
             user.email = email
+        }
+
+        if (await updatePasswordCheck(currentPassword, newPassword, user)) {
+            user.password = await hash( newPassword, 8 )
         }
     }
 }
@@ -24,6 +30,21 @@ async function updateEmailCheck (email, user) {
 
         if (userWithEmail && userWithEmail.id !== user.id) {
             throw new appError (" This email is not available!")
+        }
+        return true
+    }
+    return false
+}
+
+async function updatePasswordCheck (currentPassword, newPassword, user) {
+    if (newPassword) {
+
+        if (!currentPassword) {
+            throw new appError ("You must provide your current password if you want to change it!")
+        }
+        const passwordCheck = await compare(currentPassword, user.password)
+        if (!passwordCheck) {
+            throw new appError ("The password isn't right!")
         }
         return true
     }
