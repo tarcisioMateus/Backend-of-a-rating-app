74d77379e5235adf3a2c56ced471e727efadefb6
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-20 18:53:15 -0200

    AdminController.js created SR_func addAlbumInputValidation{}

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -5,7 +5,27 @@ const appError = require('../utils/appError')
 class AdminController {
     async addAlbum (request, response) {
         const { title, singer, genre, record_lable, release_year } = request.body
+
+        addAlbumInputValidation ( title, singer, genre, record_lable, release_year )
     }
 }
 
 module.exports = AdminController
+
+function addAlbumInputValidation ( title, singer, genre, record_lable, release_year ) {
+    if (!title) {
+        throw new appError("You can NOT add an album without it's title!")
+    }
+    if (!singer) {
+        throw new appError("You can NOT add an album without it's singer!")
+    }
+    if (!genre) {
+        throw new appError("You can NOT add an album without it's genre!")
+    }
+    if (!record_lable) {
+        throw new appError("You can NOT add an album without it's record lable!")
+    }
+    if (!release_year) {
+        throw new appError("You can NOT add an album without it's release year!")
+    }
+}
