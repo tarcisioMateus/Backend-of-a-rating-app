8d8e49bb4dcb59f69a17e866c3013c41de728570
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-20 18:42:04 -0200

    AdminController.js created method addAlbum{}

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -3,7 +3,9 @@ const knex = require('../database/knex')
 const appError = require('../utils/appError')
 
 class AdminController {
-
+    async addAlbum (request, response) {
+        const { title, singer, genre, record_lable, release_year } = request.body
+    }
 }
 
 module.exports = AdminController
