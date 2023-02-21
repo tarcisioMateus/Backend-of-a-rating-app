87b6f586e96527ba502df9048f1b359528bf29e7
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-21 12:21:31 -0200

    RatingsController.js created SR_func createInputValidation{}

--- src/controllers/RatingsController.js
+++ src/controllers/RatingsController.js
@@ -7,8 +7,21 @@ class RatingsController {
         const { album_id, user_id } = request.params
         const { stars, review } = request.body
 
-        
+        createInputValidation (stars, review)
     }
 }
 
 module.exports = RatingsController
+
+function createInputValidation (stars, review) {
+
+    if (!stars) {
+        throw new appError('please, tell us how many stars would you give this album?')
+    }
+    if (stars < 0 || stars > 5) {
+        throw new appError('the amount of stars you can give in is between 0 and 5 only!')
+    }
+    if (!review) {
+        throw new appError('please, tell us what did you think of this album!')
+    }
+}
