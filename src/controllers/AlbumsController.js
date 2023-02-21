9ea6ba2651d5fc6708f921d733ade0a64ffd86df
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-20 22:22:50 -0200

    AlbumsController.js created method listAllTags{}

--- src/controllers/AlbumsController.js
+++ src/controllers/AlbumsController.js
@@ -3,7 +3,35 @@ const knex = require('../database/knex')
 const appError = require('../utils/appError')
 
 class AlbumsController {
+    async listAllTags (request, response) {
+        const albums = await knex('albums')
 
+        const singers = []
+        const genres = []
+        const record_lables = []
+        const release_years = []
+        for (const album of albums) {
+            if (!singers.includes(album.singer)) {
+                singers = [ ...singers, album.singer]
+            }
+            if (!genres.includes(album.genre)) {
+                genres = [ ...genres, album.genre]
+            }
+            if (!record_lables.includes(album.record_lable)) {
+                record_lables = [ ...record_lables, album.record_lable]
+            }
+            if (!release_years.includes(album.release_year)) {
+                release_years = [ ...release_years, album.release_year]
+            }
+        }
+
+        return response.json({
+            singers,
+            genres,
+            record_lables,
+            release_years
+        })
+    }
 }
 
 module.exports = AlbumsController
