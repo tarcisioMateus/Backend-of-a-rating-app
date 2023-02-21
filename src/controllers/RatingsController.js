dd7083e696770f66448597487fd674c58e755f6a
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-21 12:10:03 -0200

    RatingsController.js created method create{}

--- src/controllers/RatingsController.js
+++ src/controllers/RatingsController.js
@@ -3,7 +3,12 @@ const knex = require('../database/knex')
 const appError = require('../utils/appError')
 
 class RatingsController {
+    async create (request, response) {
+        const { album_id, user_id } = request.params
+        const { stars, review } = request.body
 
+        
+    }
 }
 
 module.exports = RatingsController
