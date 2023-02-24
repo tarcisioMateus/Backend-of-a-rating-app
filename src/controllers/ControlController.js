726d9c5ce2d3437b6b26a84881ce3f678f488149
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-24 12:22:12 -0200

    ControlController.js created method listActivityDays{}

--- src/controllers/ControlController.js
+++ src/controllers/ControlController.js
@@ -3,7 +3,13 @@ const knex = require('../database/knex')
 const appError = require('../utils/appError')
 
 class ControlController {
+    listActivityDays (request, response) {
+        const { days } = request.body
 
+        const ratings = await knex('ratings')
+        
+        
+    }
 }
 
 module.exports = ControlController
