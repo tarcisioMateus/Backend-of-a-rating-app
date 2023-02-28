26bd4b377c2548fc4f170f86bd7091b80998821d
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-02-28 11:00:05 -0300

    created migrations/...history.js

---
+++ src/database/knex/migrations/20230228132441_history.js
@@ -0,0 +1,11 @@
+exports.up = knex => knex.schema.createTable('history', table => {
+    table.increments('id')
+    table.integer('user_id').references('id').inTable('users')
+
+    table.text('type').notNullable()
+    table.text('data').notNullable()
+
+    table.timestamp('date_time').default(knex.fn.now())
+})
+
+exports.down = knex => knex.schema.dropTable('history')
