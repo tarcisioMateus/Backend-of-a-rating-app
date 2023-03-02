858beb006a2a5aac0b31becd3b4e8945ac9048b3
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-03-02 13:51:17 -0300

    HistoryController.js created index{}

--- src/controllers/HistoryController.js
+++ src/controllers/HistoryController.js
@@ -3,7 +3,33 @@ const knex = require('../database/knex')
 const appError = require('../utils/appError')
 
 class HistoryController {
+    async index (request, response) {
+        const { type, who } = request.body
+        const { admin_id } = request.params
 
+        let history
+
+        if (type) {
+            history = await knex('history').select([
+                'id', 'user_id', 'type', 'date_time'
+            ]).whereLike('type', `%${type}%`)
+        } else {
+            history = await knex('history').select([
+                'id', 'user_id', 'type', 'date_time'
+            ])
+        }
+
+        switch (who) {
+            case 'mine':
+                history = history.filter(h => h.user_id == admin_id)
+                break;
+            case 'others':
+                history = history.filter(h => h.user_id != admin_id)
+                break;
+        }
+
+        return response.json(history)
+    }
 }
 
 module.exports = HistoryController
